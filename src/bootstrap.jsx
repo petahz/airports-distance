c20445a9f2aad7dc07c100b35fabb415fff76c14
Peter Tuan <xpetahzx@gmail.com>
2016-09-26 14:04:16 -0400

    Required for material-ui, a themeprovider on app bootstrap

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// Needed for onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 import configureStore from './redux/store/configure-store';
 import { INITIAL_STATE } from './common/app-const';
 import babel from 'babel-polyfill';
 import styles from './scss/style.scss';
 
 ReactDOM.render(
-    <Root store={configureStore(INITIAL_STATE)} />,
+    <MuiThemeProvider>
+        <Root store={configureStore(INITIAL_STATE)} />
+    </MuiThemeProvider>,
     document.getElementById('example-app')
 );
